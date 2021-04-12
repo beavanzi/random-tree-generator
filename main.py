8d6906af323529bbac65f3729894448f068647b1
beavanzi <bia.avanzi@gmail.com>
2021-04-12 20:21:00 -0300

    feat: add random tree random walk function

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from typing import List
-import time
 import timeit
+import random
 
-fileTreeName = "arvore-0"
+
+# Beatriz Avanzi Ecli       RA 108612
+# Sarah Anduca              RA 115506
 
 class Vertice:
     def __init__(self, num: int) -> None:
         self.d = None
+        self.f = None
         self.cor = None
         self.pai = None
-        self.adj = None
+        self.visited = False
         self.num = num
         self.adj: List[Vertice] = []
 
@@ -18,86 +21,131 @@ class Grafo:
     def __init__(self, n: int) -> None:
         self.vertices = [Vertice(i) for i in range(n)]
         self.numberOfVertices = n
-        self.diameterAssertion = None
+        self.numberOfEdges = 0
+        self.isConnected = True
 
-    def addAresta(self, u: int, v: int):
+    def addEdge(self, u: int, v: int):
         self.vertices[u].adj.append(self.vertices[v])
         self.vertices[v].adj.append(self.vertices[u])
+        self.numberOfEdges = self.numberOfEdges + 1
 
+    # Pega o "id" de um vertice aleatorio do grafo
+    def pickRandomVertexId(self) -> int:
+        n: int = random.randrange(len(self.vertices))
+        return n
 
-def InitializeAllVertex(T: Grafo) -> Grafo:
-    for v in T.vertices:
-        v.d = -1
-        v.pai = None
-        v.cor = "branco"
-    return T
+    # Inicialização dos vertices
+    def initializeAllVertex(self):
+        for v in self.vertices:
+            v.d = -1
+            v.pai = None
+            v.cor = "branco"
+            v.f = -1
 
+    # BFS
+    def BFS(self, s: Vertice):
+        self.initializeAllVertex()
+        s.d = 0
+        s.cor = "cinza"
+        Q = [s]
+        u = None
 
-def LongestPathBFS(T: Grafo, s: Vertice) -> Vertice:
-    T: Grafo = InitializeAllVertex(T)
-    s.d = 0
-    s.cor = "cinza"
-    Q = [s]
-    u = None
+        while Q:
+            u = Q.pop(0)
+            for v in self.vertices[u.num].adj:
+                if v.cor == "branco":
+                    v.cor = "cinza"
+                    v.d = u.d + 1
+                    v.pai = u
+                    Q.append(v)
+            u.cor = "preto"
 
-    while Q:
-        u = Q.pop(0)
-        for v in T.vertices[u.num].adj:
+    # Verifica se é uma arvore
+    def isTree(self):
+        # ve se arestas = vertices - 1
+        if not self.numberOfEdges == self.numberOfVertices - 1:
+            return False
+
+        # ve se é conexo
+        x: int = self.pickRandomVertexId()
+        s: Vertice = self.vertices[x]
+
+        self.BFS(s)
+
+        self.isConnected = True
+
+        for v in self.vertices:
             if v.cor == "branco":
-                v.cor = "cinza"
-                v.d = u.d + 1
-                v.pai = u
-                Q.append(v)
-        u.cor = "preto"
+                self.isConnected = False
 
-    return u
+        if self.isConnected:
+            return True
+
+        return False
 
 
-def Diameter(T) -> int:
-    s: Vertice = T.vertices[0]
-    a: Vertice = LongestPathBFS(T, s)
-    b: Vertice = LongestPathBFS(T, a)
+# Retorna o ultimo vertice de um grafo
+def GetLastVertex(graph: Grafo) -> Vertice:
+    v: Vertice = graph.vertices[len(graph.vertices)]
+    return v
+
+
+# Geração de arvore aleatoria
+def RandomTreeRandomWalk(n: int) -> Grafo:
+    graph: Grafo = Grafo(n)
+    for u in graph.vertices:
+        u.visited = False
+
+    x: int = graph.pickRandomVertexId()
+    u: Vertice = graph.vertices[x]
+    u.visited = True
+    while graph.numberOfEdges < n - 1:
+        y: int = graph.pickRandomVertexId()
+        v: Vertice = graph.vertices[y]
+        if not v.visited:
+            graph.addEdge(u.num, v.num)
+            v.visited = True
+        u = v
+    return graph
+
+
+# Calculo do diameter
+def Diameter(tree: Grafo) -> int:
+    n: int = tree.pickRandomVertexId()  # pega um numero aleatorio no range da quantidade de vertices
+    s: Vertice = tree.vertices[n]
+
+    tree.BFS(s)
+    a: Vertice = GetLastVertex(tree)
+    tree.BFS(a)
+    b: Vertice = GetLastVertex(tree)
     return b.d
-
-
-def addEdgeFromFile(G: Grafo, line):
-    spt = line.split()
-    a: int = int(spt[0])
-    b: int = int(spt[1])
-    G.addAresta(a, b)
-    return G
-
-
-def createGraph(path):
-    try:
-        f = open(path, 'r')
-    except FileNotFoundError:
-        print("Arquivo inacessível...")
-        return None
-    numberOfVertices = int(f.readline())
-    diameter = int(f.readline())
-
-    G: Grafo = Grafo(numberOfVertices)
-
-    line = f.readline()
-    while line != '':
-        addEdgeFromFile(G, line)
-        line = f.readline()
-    f.close()
-
-    G.diameterAssertion = diameter
-    return G
 
 
 def main():
     inicio = timeit.default_timer()
 
-    T = createGraph("./tests-files/" + fileTreeName + ".txt")
-    d = Diameter(T)
-    assert d == T.diameterAssertion
+    for i in range(1000):
+        graph250: Grafo = RandomTreeRandomWalk(250)
+        graph500: Grafo = RandomTreeRandomWalk(500)
+        graph750: Grafo = RandomTreeRandomWalk(750)
+        graph1000: Grafo = RandomTreeRandomWalk(1000)
+        graph1250: Grafo = RandomTreeRandomWalk(1250)
+        graph1500: Grafo = RandomTreeRandomWalk(1500)
+        graph1750: Grafo = RandomTreeRandomWalk(1750)
+        graph2000: Grafo = RandomTreeRandomWalk(2000)
+        assert graph250.isTree()
+        assert graph500.isTree()
+        assert graph750.isTree()
+        assert graph1000.isTree()
+        assert graph1250.isTree()
+        assert graph1500.isTree()
+        assert graph1750.isTree()
+        assert graph2000.isTree()
 
     fim = timeit.default_timer()
-    print('duracao: %f' % (fim - inicio))
+
+    # mostra o tempo que demorou para execução
+    print('Calculo da duracao: %f' % (fim - inicio))
 
 
 if __name__ == '__main__':
